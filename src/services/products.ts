91b53a7d606f54f8b2df40be9a4c4b551fc49331
Emerson Warhman <edwarhman@gmail.com>
2023-09-12 18:29:40 -0400

    add search product service and show only coincidence in products page

--- src/services/products.ts
+++ src/services/products.ts
@@ -8,3 +8,10 @@ export const getProductById = async ({ id }: { id: string }): Promise<typeof pro
   console.log({})
   return products.find((el) => el.id.toString() === id)
 }
+
+export const searchProducts = async ({ search }: { search: string }): Promise<typeof products> => {
+  return products.filter((product) => {
+    const values = Object.values(product)
+    return values.join(' ').toLowerCase().match(new RegExp(`${search}`))
+  })
+}
