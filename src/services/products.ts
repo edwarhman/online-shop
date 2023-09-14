ee911c4df5baf1aafb80d054e6351eeb87e3d6b0
Emerson Warhman <edwarhman@gmail.com>
2023-09-14 14:49:55 -0400

    format code using prettier

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,16 +1,27 @@
 import { products } from '../database/products.json'
 
 export const getAllProducts = async (): Promise<typeof products> => {
-  return products
+	return products
 }
 
-export const getProductById = async ({ id }: { id: string }): Promise<typeof products[0] | undefined> => {
-  return products.find((el) => el.id.toString() === id)
+export const getProductById = async ({
+	id
+}: {
+	id: string
+}): Promise<(typeof products)[0] | undefined> => {
+	return products.find((el) => el.id.toString() === id)
 }
 
-export const searchProducts = async ({ search }: { search: string }): Promise<typeof products> => {
-  return products.filter((product) => {
-    const values = Object.values(product)
-    return values.join(' ').toLowerCase().match(new RegExp(`${search}`))
-  })
+export const searchProducts = async ({
+	search
+}: {
+	search: string
+}): Promise<typeof products> => {
+	return products.filter((product) => {
+		const values = Object.values(product)
+		return values
+			.join(' ')
+			.toLowerCase()
+			.match(new RegExp(`${search}`))
+	})
 }
