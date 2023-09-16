7f242459014abc61554c82b6cf2c8cfa3f1f1228
Emerson Warhman <edwarhman@gmail.com>
2023-09-16 01:18:22 -0400

    change theme and add text logo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {}
+		extend: {},
+		fontFamily: {
+			'pacifico': ['Pacifico', 'cursive']
+		}
 	},
 	plugins: []
 }
