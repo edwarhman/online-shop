b1b4a92efc7e0afdb2846e38a85d2852f1f82131
Emerson Warhman <edwarhman@gmail.com>
2023-09-30 16:52:11 -0400

    use hybrid output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   integrations: [tailwind()],
   adapter: vercel()
 });
