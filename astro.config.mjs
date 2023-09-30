cb101c4cabe263c44a4c03e8ed141ad121f8dff7
Emerson Warhman <edwarhman@gmail.com>
2023-09-30 16:58:06 -0400

    Revert "use hybrid output"
    This reverts commit b1b4a92efc7e0afdb2846e38a85d2852f1f82131.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   integrations: [tailwind()],
   adapter: vercel()
 });
