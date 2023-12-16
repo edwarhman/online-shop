07b5c97d9decbe1e466de1fd45779268b384034b
Emerson Warhman <edwarhman@gmail.com>
2023-12-16 10:12:23 -0400

    add product images gallery component

---
+++ src/components/preact/ProductGallery.tsx
@@ -0,0 +1,36 @@
+import { useState } from "preact/hooks";
+
+interface ProductGallery {
+    images: string[];
+}
+
+function ProductGallery({
+    images
+}: ProductGallery) {
+
+    const [current, setCurrent] = useState(0)
+
+    return (<div class='grid'>
+        <div>
+            <img
+                src={images[current]} 
+                alt="selected product image" 
+                class='w-full h-80 img object-cover'
+            />
+        </div>
+        <ul class='flex gap-4 mt-4'>
+            {
+                images.map((image, idx)=> (
+                <li class={`${idx === current ? 'border-gray-500' : ''} border`}>
+                    <a onClick={()=> {
+                        setCurrent(idx)
+                    }} class='cursor-pointer'>
+                        <img class='object-cover h-20 aspect-square' src={image} />    
+                    </a>
+                </li>
+            ))}
+        </ul>
+    </div>)
+}
+
+export default ProductGallery
